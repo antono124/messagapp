9dcc008cf347355982f47bde9b60e3c67ee7adfb
George Ant <g.antonopoulos000@gmail.com>
2015-10-09 16:06:38 +0300

    Delete unnecessary methods from hibernateUtil

--- src/main/java/com/antogeo/dao/HibernateUtil.java
+++ src/main/java/com/antogeo/dao/HibernateUtil.java
@@ -13,30 +13,10 @@ public class HibernateUtil {
     @Autowired
     private SessionFactory sessionFactory;
 
-    protected Object insert(Object o) throws RuntimeException{
-        Session session =   getSessionFactory().getCurrentSession();
+    protected Object insert(Object o) throws RuntimeException {
+        Session session = getSessionFactory().getCurrentSession();
         session.save(o);
         return o;
-    }
-
-    protected Object update(Object o) throws RuntimeException{
-
-        Session session = getSessionFactory().getCurrentSession();
-        session.update(o);
-        return o;
-    }
-
-
-    protected List<Object> selectAll(String objectType){
-        try{
-            Session session = getSessionFactory().getCurrentSession();
-
-            List<Object> objects = (List<Object>) session.createQuery("FROM " + objectType).list();
-            return objects;
-        }catch(RuntimeException e){
-            getSessionFactory().getCurrentSession().getTransaction().rollback();
-            throw e;
-        }
     }
 
     protected Object select(String objectType, String column, String value){
@@ -48,25 +28,6 @@ public class HibernateUtil {
             List<Object> objects = session.createQuery(query).setParameter("value", value).list();
 
             return objects.get(0);
-
-        }catch(RuntimeException e){
-            getSessionFactory().getCurrentSession().getTransaction().rollback();
-            System.out.println("Transaction rollback");
-            throw e;
-        }
-    }
-
-
-    protected Object selectListById(String objectType, String column, long value){
-        try{
-
-            String query = "FROM " + objectType + " A WHERE A." + column + " = :value ";
-
-            Session session = getSessionFactory().getCurrentSession();
-
-            List<Object> objects = session.createQuery(query).setLong("value", value).list();
-
-            return objects;
 
         }catch(RuntimeException e){
             getSessionFactory().getCurrentSession().getTransaction().rollback();
